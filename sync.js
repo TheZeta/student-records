a0042d91cc1b48a9fedcab7ca4bf2cd456bf0ab7
Zafer Engin <efekanzafer.engin@gmail.com>
2024-07-11 22:17:14 +0300

    Add database sync function

---
+++ sync.js
@@ -0,0 +1,18 @@
+const sequelize = require('./config/database');
+const Record = require('./models/Record');
+
+const syncDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
+        await sequelize.sync({ force: true });
+        console.log('All models were synchronized successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    } finally {
+        await sequelize.close();
+    }
+};
+
+syncDatabase();
