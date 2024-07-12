4f57caebb77ab80a40cc913e40076982d8cd66c9
Zafer Engin <efekanzafer.engin@gmail.com>
2024-07-12 15:57:58 +0300

    Delete incorrect validation for student number

--- middleware/validateRecord.js
+++ middleware/validateRecord.js
@@ -14,8 +14,6 @@ const validateRecord = [
   body('stdNumber')
     .matches(/^B\d{3}X\d{5}$/)
     .withMessage('Student number must follow the pattern B###X#####')
-    .exists()
-    .withMessage('A student record with the same student number already exists')
     .notEmpty()
     .withMessage('Student number is required'),
   body('grades').isArray().withMessage('Grades must be an array'),
